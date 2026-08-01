4e2555d40e6a61a120a18b54dfe7ab68701ec6dc
Willie-Habimana <132857565+Willie-Habimana@users.noreply.github.com>
2026-07-31 23:37:35 -0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hmap = defaultdict(list)
+
+        for s in strs:
+            hmap[''.join(sorted(s))].append(s)
+        
+        return list(hmap.values())
+        
+
+        
