05c25330a46b16bbcbe6767cb7e16e5d63ff45ea
Willie-Habimana <132857565+Willie-Habimana@users.noreply.github.com>
2026-08-03 12:53:06 -0700

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        earliest = len(nums) - 1
+
+
+        for i in range(len(nums) -1, -1, -1):
+            if i + nums[i] >= earliest:
+                earliest = i
+        
+        return True if earliest == 0 else False
+
+            
+
+            
+
+
+
+        
